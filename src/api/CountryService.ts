5085ad8316f0eaf36ddc3f33ee1d349d8188a787
aboshady <ahmed.aboshady404@gmail.com>
2025-02-02 15:02:02 +0200

    add more country services

--- src/api/CountryService.ts
+++ src/api/CountryService.ts
@@ -1,7 +1,9 @@
 
+import { CountryState } from "@app/types/CountryState";
 import {callApi} from "./axios";
 import endPoints from "./Endpoints";
 import { Country } from "@app/types/Country";
+
 
 
 
@@ -9,6 +11,26 @@ export const getCountryByName = async (name: string): Promise<Country> => {
   try {
       const response = await callApi(endPoints.getCountryByName, {
         urlParams: {name}
+      });
+      return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const listCountries = async (): Promise<Country[]> => {
+  try {
+      const response = await callApi(endPoints.listCountries, {});
+      return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const listCountryStates = async (code: string): Promise<CountryState[]> => {
+  try {
+      const response = await callApi(endPoints.listCountryStates, {
+        urlParams: {code}
       });
       return response;
   } catch (error) {
